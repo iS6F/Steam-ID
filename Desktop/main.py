af1c3cece6f3bca3de386e13cfcbf6f335175f9d
iS6F <66199912+iS6F@users.noreply.github.com>
2022-07-08 11:12:19 +0400

    Create main.py

---
+++ Desktop/main.py
@@ -0,0 +1,15 @@
+import requests, random, string
+from bs4 import BeautifulSoup as bs
+length = 3
+webhook = ""
+while True:
+    id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+    request = requests.get(f'https://steamcommunity.com/id/{id}')
+    lxml = bs(request.content, 'lxml')
+    title = lxml.find('title')
+    list = title.text.split()
+    if list[-1] == "Error":
+        print(f'{id} is Available')
+        requests.post(webhook, data={"content" : f'{id} is Available'})
+    else:
+        print(f'{id} is Taken')
